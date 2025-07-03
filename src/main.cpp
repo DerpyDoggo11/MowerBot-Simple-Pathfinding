f1a72c06d5c239d64d94f9c78b1aa42c74040b2f
DerpyDoggo11 <vrs18451@gmail.com>
2025-07-03 10:40:19 -0700

    simplified code

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ const int BRAKE_PIN_2 = 14;
 const int SPEED_PIN_2 = 13;
 const int PWM_PIN_2 = 12;
 
+const int DEBUG_LED = 2;
+
 // Set these to your desired credentials.
 const char *ssid = "Mowerbot";
 const char *password = "Password";
@@ -22,8 +24,10 @@ NetworkServer server(80);
 
 
 
-// put function declarations here:
-int myFunction(int, int);
+void stopMotors();
+void moveForward();
+void turnLeft();
+void turnRight();
 
 void setup() {
   pinMode(2, OUTPUT); // debug light
@@ -48,82 +52,68 @@ void setup() {
 
   Serial.println("Server started");
 }
-
 void loop() {
-  NetworkClient client = server.accept();  // listen for incoming clients
+  WiFiClient client = server.accept();
 
   if (client) {
-    Serial.println("New Client.");
-    String currentLine = ""; 
+    Serial.println("Client connected");
 
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
+    String request = client.readStringUntil('\r');
+    Serial.println("Request: " + request);
 
-            // Enlarged Text Styling
-            client.println("<html><body>");
-            client.println("<h1 style='font-size:100px;'>ESP32 Motor Control</h1>");
-            client.println("<p style='font-size:50px;'>Click <a href='/N'>here</a> to turn ON the motors.</p>");
-            client.println("<p style='font-size:50px;'>Click <a href='/F'>here</a> to turn OFF the motors.</p>");
+    // 🌐 Serve HTML
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html\n");
+    client.println("<html><body><h1>ESP32 Motor Control</h1>");
+    client.println("<a href='/N'>ON</a><br>");
+    client.println("<a href='/F'>OFF</a><br>");
+    client.println("<a href='/L'>LEFT</a><br>");
+    client.println("<a href='/R'>RIGHT</a><br>");
+    client.println("</body></html>");
 
-            client.println("<p style='font-size:50px;'>Click <a href='/R'>here</a> to turn Right.</p>");
-            client.println("<p style='font-size:50px;'>Click <a href='/L'>here</a> to turn Left.</p>");
-            client.println("</body></html>");
+    // ⚙️ Command Handling
+    if (request.indexOf("GET /N") != -1) moveForward();
+    else if (request.indexOf("GET /F") != -1) stopMotors();
+    else if (request.indexOf("GET /L") != -1) turnLeft();
+    else if (request.indexOf("GET /R") != -1) turnRight();
 
-            client.println();
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;
-        }
-
-        // Check client request
-        if (currentLine.endsWith("GET /N")) { // on
-          digitalWrite(2, HIGH); // debug light
-          analogWrite(PWM_PIN_1, 40);
-          analogWrite(PWM_PIN_2, 40);
-          digitalWrite(DIR_PIN_1, HIGH);
-          digitalWrite(DIR_PIN_2, HIGH);
-        }
-        if (currentLine.endsWith("GET /F")) { // off
-          digitalWrite(2, LOW); // debug light
-          analogWrite(PWM_PIN_1, 0);
-          analogWrite(PWM_PIN_2, 0);
-          digitalWrite(DIR_PIN_1, LOW);
-          digitalWrite(DIR_PIN_2, LOW);
-        }
-        if (currentLine.endsWith("GET /R")) { // off
-          digitalWrite(2, HIGH); // debug light
-          analogWrite(PWM_PIN_1, 40);
-          analogWrite(PWM_PIN_2, 40);
-          digitalWrite(DIR_PIN_1, LOW);
-          digitalWrite(DIR_PIN_2, HIGH);
-        }
-        if (currentLine.endsWith("GET /L")) { // off
-          digitalWrite(2, HIGH); // debug light
-          analogWrite(PWM_PIN_1, 40);
-          analogWrite(PWM_PIN_2, 40);
-          digitalWrite(DIR_PIN_1, HIGH);
-          digitalWrite(DIR_PIN_2, LOW);
-        }
-      }
-    }
-    
     client.stop();
-    Serial.println("Client Disconnected.");
+    Serial.println("Client disconnected");
   }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void stopMotors() {
+  analogWrite(PWM_PIN_1, 0);
+  analogWrite(PWM_PIN_2, 0);
+  digitalWrite(DIR_PIN_1, LOW);
+  digitalWrite(DIR_PIN_2, LOW);
+
+  digitalWrite(DEBUG_LED, LOW);
+}
+
+void moveForward() {
+  analogWrite(PWM_PIN_1, 40);
+  analogWrite(PWM_PIN_2, 40);
+  digitalWrite(DIR_PIN_1, HIGH);
+  digitalWrite(DIR_PIN_2, HIGH);
+
+  digitalWrite(DEBUG_LED, HIGH);
+}
+
+void turnLeft() {
+  analogWrite(PWM_PIN_1, 40);
+  analogWrite(PWM_PIN_2, 40);
+  digitalWrite(DIR_PIN_1, HIGH);
+  digitalWrite(DIR_PIN_2, LOW);
+
+  digitalWrite(DEBUG_LED, HIGH);
+}
+
+void turnRight() {
+  analogWrite(PWM_PIN_1, 40);
+  analogWrite(PWM_PIN_2, 40);
+  digitalWrite(DIR_PIN_1, LOW);
+  digitalWrite(DIR_PIN_2, HIGH);
+
+  digitalWrite(DEBUG_LED, HIGH);
 }
